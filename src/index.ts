dbed5aa02aed30f6a9efd9fca3c586f394a5a1c3
Nihal <nihal04x@gmail.com>
2026-06-08 15:38:34 +0530

    wired up new routes in index and added new endpoints in api test file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import employeeRoutes from './routes/employee.routes'
 import { logger } from 'hono/logger'
+import leaveRoutes from './routes/leave.routes'
+import authRoutes from './routes/auth.routes'
 
 const app = new Hono()
 
@@ -10,6 +12,8 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.route("/auth", authRoutes)
 app.route("/employee", employeeRoutes)
+app.route("/leave", leaveRoutes)
 
 export default app
