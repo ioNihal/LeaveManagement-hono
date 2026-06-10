282a4c86c75e4bbe5d5c9d56645277f1efb40460
Nihal <nihal04x@gmail.com>
2026-06-10 18:09:26 +0530

    updated audit service - To use transaction as second param, if no transaction is recived, it wil use db as fallback executer. - Typed it using union with database type or PgTransaction generic type

--- src/services/audit.service.ts
+++ src/services/audit.service.ts
@@ -1,5 +1,9 @@
-import { db } from "../db";
+import { ExtractTablesWithRelations } from "drizzle-orm";
+import { Database, db } from "../db";
 import { auditLogTable } from "../db/schema";
+import { PostgresJsQueryResultHKT } from "drizzle-orm/postgres-js";
+import { PgTransaction } from "drizzle-orm/pg-core";
+
 
 export type AuditLogType = {
     leaveRequestId: string;
@@ -7,9 +11,12 @@ export type AuditLogType = {
     action: string;
 }
 
+type TxType = Database | PgTransaction<PostgresJsQueryResultHKT, Record<string, never>, ExtractTablesWithRelations<Record<string, never>>>
+
 export const auditServices = {
-    async log(data: AuditLogType) {
-        const [log] = await db
+    async log(data: AuditLogType, tx?: TxType) {
+        const executor = tx ?? db;
+        const [log] = await executor
             .insert(auditLogTable)
             .values(data)
             .returning();
