1c9e2f8857be4647419642b29da544c41c307d57
Nihal <nihal04x@gmail.com>
2026-06-10 18:06:13 +0530

    added logger for SQL queries and exported Database type

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,4 +4,6 @@ import postgres from 'postgres'
 
 const client = postgres(Bun.env.DATABASE_URL!)
 
-export const db = drizzle(client)
+export type Database = typeof db
+
+export const db = drizzle(client, { logger : true})
